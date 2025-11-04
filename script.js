dad4b2859553ae9a449965a09d9b9382a3eb62f9
L1m3Kun <sungku2757@gmail.com>
2025-11-04 13:49:34 +0900

    event 객체 확인

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const section = document.querySelectorAll("section");
   section.forEach((sec) => {
-    sec.addEventListener("click", () => {
+    sec.addEventListener("click", (event) => {
+      console.log(event);
       sec.classList.toggle("highlight");
     });
   });
